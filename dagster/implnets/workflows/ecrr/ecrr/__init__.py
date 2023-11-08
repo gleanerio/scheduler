49843bfd10b40836068bdc9e02aaef2190bedfbe
David Valentine <david.valentine@gmail.com>
2023-11-07 17:54:41 -0800

    update ecrr code

--- dagster/implnets/workflows/ecrr/ecrr/__init__.py
+++ dagster/implnets/workflows/ecrr/ecrr/__init__.py
@@ -1,12 +1,12 @@
 from dagster import repository, Definitions
 
-from .jobs.implnet_jobs_ecrr_submitted import implnet_job_ecrr_submitted
+from .jobs.implnet_jobs_ecrr_submitted import job_ecrr_submitted
 from .sch.implnet_sch_ecrr_submitted  import implnet_sch_ecrr_submitted
-from .jobs.implnet_jobs_ecrr_examples import implnet_job_ecrr_examples
+from .jobs.implnet_jobs_ecrr_examples import job_ecrr_examples
 from .sch.implnet_sch_ecrr_examples  import implnet_sch_ecrr_examples
 
 
-jobs = [ implnet_job_ecrr_submitted, implnet_job_ecrr_examples]
+jobs = [ job_ecrr_submitted, job_ecrr_examples]
 schedules = [ implnet_sch_ecrr_submitted, implnet_sch_ecrr_examples]
 
 defs = Definitions(
